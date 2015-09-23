c35bf878eff1e1a21903e165c5675e1aa7fefd33
José Loguercio <jloguercio.work@gmail.com>
2015-09-23 16:56:04 -0500

    remove var_dump variable for testing - Refs #7841

--- main/inc/lib/elfinder/filemanager.php
+++ main/inc/lib/elfinder/filemanager.php
@@ -4,6 +4,5 @@
 require_once '../../global.inc.php';
 
 Chat::setDisableChat();
-var_dump($_SESSION['disable_chat']);
 $template = new Template();
 $template->display('default/javascript/editor/ckeditor/elfinder.tpl');
