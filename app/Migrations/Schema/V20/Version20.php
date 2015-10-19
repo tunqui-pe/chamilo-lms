5cf26125e0d3982166575ef085e38c59037623b4
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-10-19 16:47:18 -0500

    Move methods from  migration V20 to new migration class - refs #7901

--- app/Migrations/Schema/V20/Version20.php
+++ app/Migrations/Schema/V20/Version20.php
@@ -18,19 +18,6 @@ class Version20 extends AbstractMigrationChamilo
      */
     public function up(Schema $schema)
     {
-        $user = $schema->getTable('user');
-        $skill = $schema->getTable('skill');
-        $course = $schema->getTable('course');
-        $session = $schema->getTable('session');
-
-        $skillRelUser = $schema->getTable('skill_rel_user');
-        $skillRelUser->getColumn('session_id')->setNotnull(false);
-        $skillRelUser->addForeignKeyConstraint($user, ['user_id'], ['id'], [], 'FK_su_user');
-        $skillRelUser->addForeignKeyConstraint($skill, ['skill_id'], ['id'], [], 'FK_su_skill');
-        $skillRelUser->addForeignKeyConstraint($course, ['course_id'], ['id'], [], 'FK_su_course');
-        $skillRelUser->addForeignKeyConstraint($session, ['session_id'], ['id'], [], 'FK_su_session');
-        $skillRelUser->addIndex(['session_id', 'course_id', 'user_id'], 'idx_select_s_c_u');
-        $skillRelUser->addIndex(['skill_id', 'user_id'], 'idx_select_sk_u');
     }
 
     /**
@@ -39,12 +26,5 @@ class Version20 extends AbstractMigrationChamilo
      */
     public function down(Schema $schema)
     {
-        $skillRelUser = $schema->getTable('skill_rel_user');
-        $skillRelUser->removeForeignKey('FK_su_user');
-        $skillRelUser->removeForeignKey('FK_su_skill');
-        $skillRelUser->removeForeignKey('FK_su_course');
-        $skillRelUser->removeForeignKey('FK_su_session');
-        $skillRelUser->dropIndex('idx_select_s_c_u');
-        $skillRelUser->dropIndex('idx_select_sk_u');
     }
 }
---
+++ app/Migrations/Schema/V20/Version20151019163400.php
@@ -0,0 +1,48 @@
+<?php
+/* For licensing terms, see /license.txt */
+namespace Application\Migrations\Schema\V20;
+
+use Application\Migrations\AbstractMigrationChamilo;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Migrations for skill assignment
+ *
+ * @author Angel Fernando Quiroz Campos <angel.quiroz@beeznest.com>
+ */
+class Version19102015163400 extends AbstractMigrationChamilo
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $user = $schema->getTable('user');
+        $skill = $schema->getTable('skill');
+        $course = $schema->getTable('course');
+        $session = $schema->getTable('session');
+
+        $skillRelUser = $schema->getTable('skill_rel_user');
+        $skillRelUser->getColumn('session_id')->setNotnull(false);
+        $skillRelUser->addForeignKeyConstraint($user, ['user_id'], ['id'], [], 'FK_su_user');
+        $skillRelUser->addForeignKeyConstraint($skill, ['skill_id'], ['id'], [], 'FK_su_skill');
+        $skillRelUser->addForeignKeyConstraint($course, ['course_id'], ['id'], [], 'FK_su_course');
+        $skillRelUser->addForeignKeyConstraint($session, ['session_id'], ['id'], [], 'FK_su_session');
+        $skillRelUser->addIndex(['session_id', 'course_id', 'user_id'], 'idx_select_s_c_u');
+        $skillRelUser->addIndex(['skill_id', 'user_id'], 'idx_select_sk_u');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $skillRelUser = $schema->getTable('skill_rel_user');
+        $skillRelUser->removeForeignKey('FK_su_user');
+        $skillRelUser->removeForeignKey('FK_su_skill');
+        $skillRelUser->removeForeignKey('FK_su_course');
+        $skillRelUser->removeForeignKey('FK_su_session');
+        $skillRelUser->dropIndex('idx_select_s_c_u');
+        $skillRelUser->dropIndex('idx_select_sk_u');
+    }
+}
