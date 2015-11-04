67c4d2fdc66be89559933737de7c2075771f6cce
Julio Montoya <gugli100@gmail.com>
2015-11-04 15:26:14 +0100

    Converts main/lang/ php files to src/ChamiloCore/Resources/translations po files

--- tests/translations/translate_all_languages_array_to_po.php
+++ tests/translations/translate_all_languages_array_to_po.php
@@ -1,17 +1,14 @@
 <?php
 /* For licensing terms, see /license.txt */
 
-require_once __DIR__.'/../../main/inc/global.inc.php';
+$sysPath = __DIR__.'/../../';
+require_once $sysPath.'main/inc/global.inc.php';
 
 //Source language do not change
-$dir = api_get_path(SYS_CODE_PATH).'lang/';
+$dir = $sysPath.'main/lang/';
 
 //Destination
-$save_path = api_get_path(SYS_PATH).'app/Resources/translations/';
-
-if (!is_dir(api_get_path(SYS_PATH).'resources')) {
-    mkdir(api_get_path(SYS_PATH).'resources');
-}
+$save_path = $sysPath.'src/Chamilo/CoreBundle/Resources/translations/';
 
 //The new po files will be saved in  $dir.'/LC_MESSAGES/';
 
@@ -23,22 +20,90 @@ if (!is_dir($save_dir_path).'/LC_MESSAGES') {
     mkdir($save_dir_path.'/LC_MESSAGES');
 }*/
 
-$englishDir = api_get_path(SYS_CODE_PATH).'lang/english';
+$englishDir = $sysPath.'main/lang/english';
 
+$languages = [
+    'arabic' => 'ar',
+    'asturian' => 'ast',
+    'basque' => 'eu',
+    'bengali' => 'bn',
+    'bosnian' => 'bs',
+    'brazilian' => 'pt-BR',
+    'bulgarian' => 'bg',
+    'catalan' => 'ca',
+    'croatian' => 'hr',
+    'czech' => 'cs',
+    'danish' => 'da',
+    'dari' => 'prs',
+    'dutch' => 'nl',
+    'english' => 'en',
+    'esperanto' => 'eo',
+    'faroese',
+    'fo',
+    'finnish' => 'fi',
+    'french' => 'fr',
+    'friulian' => 'fur',
+    'galician' => 'gl',
+    'georgian' => 'ka',
+    'german' => 'de',
+    'greek' => 'el',
+    'hebrew' => 'he',
+    'hindi' => 'hi',
+    'hungarian' => 'hu',
+    'indonesian' => 'id',
+    'italian' => 'it',
+    'japanese' => 'ja',
+    'korean' => 'ko',
+    'latvian' => 'lv',
+    'lithuanian' => 'lt',
+    'macedonian' => 'mk',
+    'malay' => 'ms',
+    'norwegian' => 'no',
+    'occitan' => 'oc',
+    'pashto' => 'ps',
+    'persian' => 'fa',
+    'polish' => 'pl',
+    'portuguese' => 'pt',
+    'quechua_cusco' => 'qu',
+    'romanian' => 'ro',
+    'russian' => 'ru',
+    'serbian' => 'sr',
+    'simpl_chinese' => 'zh',
+    'slovak' => 'sk',
+    'slovenian' => 'sl',
+    'somali' => 'so',
+    'spanish' => 'es',
+    'swahili' => 'sw',
+    'swedish' => 'sv',
+    'tagalog',
+    'tl',
+    'thai' => 'th',
+    'tibetan',
+    'bo',
+    'trad_chinese' => 'zh-TW',
+    'turkish' => 'tr',
+    'ukrainian' => 'uk',
+    'vietnamese' => 'vi',
+    'xhosa' => 'xh',
+    'yoruba' => 'yo',
+];
+$simple = ['spanish', 'french', 'english'];
 $iterator = new FilesystemIterator($dir);
 foreach ($iterator as $folder) {
     if ($folder->isDir()) {
         $langPath = $folder->getPathname();
 
-        if ($folder->getBasename() != 'english') {
+        if (in_array($folder->getBasename(), $simple)) {
             continue;
         }
 
-        echo $langPath.PHP_EOL;
         $langIterator = new FilesystemIterator($langPath);
         $filter = new RegexIterator($langIterator, '/\.(php)$/');
         foreach ($filter as $phpFile) {
             $phpFilePath = $phpFile->getPathname();
+            if ($phpFile->getBasename() != 'trad4all.inc.php') {
+                continue;
+            }
             $po = file($phpFilePath);
             $translations = array();
 
@@ -49,6 +114,10 @@ foreach ($iterator as $folder) {
                 if ($pos) {
                     $variable = (substr($line, 1, $pos - 1));
                     $variable = trim($variable);
+
+                    if ($variable == 'NameOfLang' || $variable == '0') {
+                        continue;
+                    }
 
                     require $englishFile;
                     $my_variable_in_english = $variable;
@@ -61,14 +130,17 @@ foreach ($iterator as $folder) {
                     );
                 }
             }
-
-            $code = api_get_language_isocode($folder->getBasename());
+            //$code = api_get_language_isocode($folder->getBasename());
+            $code = isset($languages[$folder->getBasename(
+                )]) ? $languages[$folder->getBasename()] : 'what';
             //LC_MESSAGES
-            $new_po_file = $save_path.$folder->getBasename().'/'.$phpFile->getBasename('.php').'.po';
+            $fileName = $phpFile->getBasename('.php');
+            $fileName = 'all';
+            $new_po_file = $save_path.'/'.$fileName.'.'.$code.'.po';
 
-            if (!is_dir($save_path.$folder->getBasename())) {
+            /*if (!is_dir($save_path.$folder->getBasename())) {
                 mkdir($save_path.$folder->getBasename());
-            }
+            }*/
 
             $fp = fopen($new_po_file, 'w');
             foreach ($translations as $item) {
