d22468a43462df8061771f2488589bb27ff64569
Julio Montoya <gugli100@gmail.com>
2015-12-02 16:10:08 +0100

    remove files

--- src/Chamilo/ThemeBundle/Resources/public/components/ckeditor/plugins/colordialog/lang/eu.js
+++
@@ -1,7 +0,0 @@
-﻿CKEDITOR.plugins.setLang("colordialog", "eu", {
-    clear: "Garbitu",
-    highlight: "Nabarmendu",
-    options: "Kolore Aukerak",
-    selected: "Hautatutako Kolorea",
-    title: "Kolorea Hautatu"
-});
