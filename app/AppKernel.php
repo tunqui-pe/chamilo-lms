e0852072996e896205c906bd0556c0b272fcae55
Julio Montoya <gugli100@gmail.com>
2015-12-03 08:49:40 +0100

    SpBowerBundle only available in test mode.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -90,6 +90,9 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        if (in_array($this->getEnvironment(), array('test'))) {
             $bundles[] = new Sp\BowerBundle\SpBowerBundle();
         }
 
