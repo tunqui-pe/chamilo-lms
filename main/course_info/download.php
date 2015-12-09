04574b6ab8ef973e2b2524153d7804c89b237be9
jmontoyaa <gugli100@gmail.com>
2015-12-09 09:13:25 +0100

    Remove unused code

--- main/course_info/download.php
+++ main/course_info/download.php
@@ -23,20 +23,7 @@ $archive_file = str_replace(array('..', '/', '\\'), '', $archive_file);
 list($extension) = getextension($archive_file);
 
 if (empty($extension) || !file_exists($archive_path.$archive_file)) {
-	exit;
-}
-
-$extension = strtolower($extension);
-$content_type = '';
-
-if (in_array($extension, array('xml', 'csv')) && (api_is_platform_admin(true) || api_is_drh())) {
-	$content_type = 'application/force-download';
-} elseif ($extension == 'zip' && $_cid && (api_is_platform_admin(true) || $is_courseAdmin)) {
-	$content_type = 'application/force-download';
-}
-
-if (empty($content_type)) {
-	api_not_allowed(true);
+    api_not_allowed(true);
 }
 
 if (Security::check_abs_path($archive_path.$archive_file, $archive_path)) {
