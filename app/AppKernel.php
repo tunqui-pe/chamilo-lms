a3cc802635d64c823e8f0ee3bd743e8ef03ba01a
Julio Montoya <gugli100@gmail.com>
2015-12-21 10:15:41 +0100

    Update jplayer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -90,9 +90,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
-
-        if (in_array($this->getEnvironment(), array('test'))) {
             //$bundles[] = new Sp\BowerBundle\SpBowerBundle();
         }
 
