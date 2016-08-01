0d91160052a870a59aaf852b94e2e6be23c4f330
jmontoyaa <gugli100@gmail.com>
2016-08-01 11:36:43 +0200

    Minor

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -140,7 +140,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            //$bundles[] = new Sp\BowerBundle\SpBowerBundle();
         }
 
         return $bundles;
