a3d4aebd17d8e5013f40cb4938a66028b533daed
jmontoyaa <gugli100@gmail.com>
2016-12-19 12:23:42 +0100

    Fix template + add missing icons

--- main/exercise/exercise_submit.php
+++ main/exercise/exercise_submit.php
@@ -156,7 +156,7 @@ if ($objExercise->review_answers) {
 }
 
 $template->assign('shuffle_answers', $objExercise->random_answers);
-$templateName = $template->get_template('exercise/submit.js.tpl');
+$templateName = $template->get_template('exercise/submit.html.twig');
 $htmlHeadXtra[] = $template->fetch($templateName);
 
 $current_timestamp = time();
