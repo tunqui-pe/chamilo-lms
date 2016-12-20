efee7ee069c215685bc319a17f569816777bd4d7
jmontoyaa <gugli100@gmail.com>
2016-12-20 09:07:47 +0100

    Add legacy lib

--- web/legacy.php
+++ web/legacy.php
@@ -4,6 +4,7 @@
 define('USERNAME_MAX_LENGTH', 40);
 
 require_once __DIR__.'/../main/inc/lib/api.lib.php';
+require_once __DIR__.'/../main/inc/lib/array.lib.php';
 require_once __DIR__.'/../main/inc/lib/database.constants.inc.php';
 require_once __DIR__.'/../main/inc/lib/internationalization.lib.php';
 require_once __DIR__.'/../main/inc/lib/text.lib.php';
