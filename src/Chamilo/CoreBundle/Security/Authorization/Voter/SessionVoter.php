d94563e955a9bc77c08b0342a95848e313c44951
jmontoyaa <gugli100@gmail.com>
2017-01-06 14:07:57 +0100

    Adding "duration" in session voter

--- src/Chamilo/CoreBundle/Security/Authorization/Voter/SessionVoter.php
+++ src/Chamilo/CoreBundle/Security/Authorization/Voter/SessionVoter.php
@@ -25,6 +25,7 @@ class SessionVoter extends Voter
 
     private $entityManager;
     private $courseManager;
+    private $container;
 
     /**
      * @param EntityManager $entityManager
@@ -62,9 +63,12 @@ class SessionVoter extends Voter
      */
     public function supports($attribute, $subject)
     {
-        return $subject instanceof Session && in_array($attribute, array(
-            self::VIEW, self::EDIT, self::DELETE
-        ));
+        $options = [
+            self::VIEW,
+            self::EDIT,
+            self::DELETE
+        ];
+        return $subject instanceof Session && in_array($attribute, $options);
     }
 
     /**
@@ -74,19 +78,20 @@ class SessionVoter extends Voter
      */
     protected function voteOnAttribute($attribute, $session, TokenInterface $token)
     {
+        /** @var User $user */
         $user = $token->getUser();
 
-        // make sure there is a user object (i.e. that the user is logged in)
+        // Make sure there is a user object (i.e. that the user is logged in)
         if (!$user instanceof UserInterface) {
-
             return false;
         }
 
-        // Checks if the current user was set up
+        // Checks if the current course was set up
+        // $session->getCurrentCourse() is set in the class CourseListener
+        /** @var Session $session */
         $course = $session->getCurrentCourse();
 
         if ($course == false) {
-
             return false;
         }
 
@@ -94,26 +99,90 @@ class SessionVoter extends Voter
 
         // Admins have access to everything
         if ($authChecker->isGranted('ROLE_ADMIN')) {
-
             return true;
         }
 
-        if (!$session->isActive()) {
-
-            return false;
-        }
+        $sessionId = $session->getId();
+        $userId = $user->getId();
 
         switch ($attribute) {
             case self::VIEW:
+                // General coach
+                $generalCoach = $session->getGeneralCoach();
+                if ($generalCoach) {
+                    $coachId = $generalCoach->getId();
+                    $userId = $user->getId();
+                    if ($coachId == $userId) {
+                        $user->addRole('ROLE_CURRENT_SESSION_COURSE_TEACHER');
+
+                        return true;
+                    }
+                }
+
+                // Course-Coach access
+                if ($session->hasCoachInCourseWithStatus($user, $course)) {
+                    if (!$session->isActiveForCoach()) {
+                        return false;
+                    }
+                    $user->addRole('ROLE_CURRENT_SESSION_COURSE_TEACHER');
+                    return true;
+                }
+
+                // Student access
                 if ($session->hasUserInCourse($user, $course)) {
                     $user->addRole('ROLE_CURRENT_SESSION_COURSE_STUDENT');
 
+                     // Session duration per student.
+                    if (!empty($session->getDuration())) {
+                        $duration = $session->getDuration() * 24 * 60 * 60;
+
+                        $courseAccess = \CourseManager::getFirstCourseAccessPerSessionAndUser(
+                            $sessionId,
+                            $userId
+                        );
+
+                        // If there is a session duration but there is no previous
+                        // access by the user, then the session is still available
+                        if (count($courseAccess) == 0) {
+                            return true;
+                        }
+
+                        $currentTime = time();
+                        $firstAccess = 0;
+                        if (isset($courseAccess['login_course_date'])) {
+                            $firstAccess = api_strtotime(
+                                $courseAccess['login_course_date'],
+                                'UTC'
+                            );
+                        }
+                        $userDurationData = \SessionManager::getUserSession(
+                            $userId,
+                            $sessionId
+                        );
+                        $userDuration = 0;
+                        if (isset($userDurationData['duration'])) {
+                            $userDuration = intval($userDurationData['duration']) * 24 * 60 * 60;
+                        }
+
+                        $totalDuration = $firstAccess + $duration + $userDuration;
+                        if ($totalDuration > $currentTime) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        if (!$session->isActiveForStudent()) {
+                            return false;
+                        }
+                    }
+
                     return true;
                 }
+
+                return false;
                 break;
             case self::EDIT:
             case self::DELETE:
-
                 // General coach check
                 $generalCoach = $session->getGeneralCoach();
                 if ($generalCoach) {
@@ -126,13 +195,16 @@ class SessionVoter extends Voter
                     }
                 }
 
-                // Course session coach check
+                // Course session check
                 if ($session->hasCoachInCourseWithStatus($user, $course)) {
-
+                    if (!$session->isActiveForCoach()) {
+                        return false;
+                    }
                     $user->addRole('ROLE_CURRENT_SESSION_COURSE_TEACHER');
 
                     return true;
                 }
+                return false;
                 break;
         }
 
