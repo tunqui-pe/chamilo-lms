afe5a2bcc68fefcc695db9bd4a6895bfcf8fe15a
jmontoyaa <gugli100@gmail.com>
2017-08-16 14:43:20 +0200

    Add webpack encore needed to load files with yarn (instead of bower)
    For more info see:
    http://symfony.com/doc/master/frontend/encore/installation.html

---
+++ public/assets/js/main.js
@@ -0,0 +1,12 @@
+const $ = require('jquery');
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+// JS is equivalent to the normal "bootstrap" package
+// no need to set this to a variable, just require it
+require('bootstrap-sass');
+var moment = require('moment');
+require('jquery-ui');
+
+require('jquery-ui-timepicker-addon');
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+
+    // read main.js     -> output as web/build/app.js
+    .addEntry('chamilo', './public/assets/js/main.js')
+    // read global.scss -> output as web/build/global.css
+    .addStyleEntry('chamilo_style', './public/assets/css/main.scss')
+
+    // enable features!
+    .enableSassLoader()
+    .autoProvidejQuery()
+    .enableReactPreset()
+    .enableSourceMaps(!Encore.isProduction())
+    .autoProvideVariables({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+    })
+    //.enableVersioning() // hashed filenames (e.g. main.abc123.js)
+;
+
+
+
+module.exports = Encore.getWebpackConfig();
