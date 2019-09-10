c111e4368ff1ee4ca846fdf7efbee19ce9228613
root <root@aula-virtual.cl>
2019-09-10 02:55:51 +0000

    add lang educachile

---
+++ main/lang/spanish2/trad4all.inc.php
@@ -0,0 +1,6 @@
+<?php
+$Phone="Teléfono o Celular";
+$SrcPhone="Teléfono o Celular";
+$ChangePasswordCurrent= "Si deseas cambiar su contraseña, deberás escribir tu contraseña actual, de lo contrario dejar el campo vacío";
+$EnterNewPassword="Ingresar tu nueva contraseña, solo si deseas cambiarla";
+$RepeatYouNewPassword="Repite tu nueva contraseña ingresada anteriormente";
