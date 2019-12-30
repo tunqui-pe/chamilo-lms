2b70deadbc1d26f4507175b670bae9c73891a327
Alex Aragón <aragcar@gmail.com>
2019-12-30 00:47:47 -0500

    add key public and private

--- plugin/buycourses/src/process_confirm.php
+++ plugin/buycourses/src/process_confirm.php
@@ -349,8 +349,57 @@ switch ($sale['payment_type']) {
             $configuration->setEnvironment(Webpay::PRODUCCION);
             //We assign the trade code
             $commerceCode = $transkbankParams['commerce_code'];
-            $privateKeyWebPay = $transkbankParams['private_key'];
-            $publicCertWebPay = $transkbankParams['public_cert'];
+            //$privateKeyWebPay = $transkbankParams['private_key'];
+            //$publicCertWebPay = $transkbankParams['public_cert'];
+            $privateKeyWebPay =  "-----BEGIN RSA PRIVATE KEY-----\n".
+                "MIIEpQIBAAKCAQEAoyOCGPMCuig5l9cEWJK0SV2Xz4QXBYAgWr8gPvXUQnuD0AxT\n".
+                "ldNZy4apMgPxE5n5obqNs5c09OSxhpOanDfK44ERySps2HVDfirNejWHL5APN/HX\n".
+                "QPsho1dehN2Vv5y8A/T9typq+fu5DW1oxY2KgczGdk6N8P4FZob6jKCzUlCSsXNM\n".
+                "L8Rm8zQa6Mm1cGr9mITtbPspW6ZHhWoJ7T3e5qBKaBefGIpV4Pia2i+5vZMLOWHM\n".
+                "dqYFEq7js53U6Gx6Co0vLMFbY3JyoEPdY98VvkSXTerHkgdwED7baKhUDXGRCLJ4\n".
+                "qrAhhBLm2jSXwlS8h8RAaS8UwHGgYVVqd5U/FwIDAQABAoIBAQCTiJUrAvnAaIhN\n".
+                "LQKdJ28ruhyEaqx3KPwZlScQSTkANrtp9vvQTyaxzMJOnQnz9Bexjwh/FYuqvPdf\n".
+                "ATWdeUden2b6SgfNaZ70Brl0f9wVw24/5dIIzhQJqAWumsXGV3QhD/ozH4VHNcKQ\n".
+                "xhUuM2bDI/GHJFtxSgiQRBlp/BqvoQQi+i9kfT8gya+hC0fLWp2pLiha2uvPkePC\n".
+                "AGCPswi1Ge4yksYZnxA45EfSrLWoiQ+rUeRzvwBl1KkquplgC4vcFl9q/eYhoZft\n".
+                "EeU8rBtwDquq0WA52m9ghJX256/oKNG+kD0leSRQzLFiUPF5n1B1qUKExqfwLyug\n".
+                "jHQIX9ohAoGBANdzQmsmAohD+a+Yh2araqR/0NaieJPrHfFwvdrsmhF+srBqdPlI\n".
+                "fEY0nB2RjO/qJ00quChoBZArXikdtJTOooMsRmO6fUMjx98/40ljIBcLn4kVTall\n".
+                "b5Cz+Xu/m/ucbBHHJQeUqPndOLvYJOM+UxXBLsyJ+zYCaBh/AZb2aIx7AoGBAMHX\n".
+                "y3M+4fkXya6Y7sSBBX4JXKQmmh8UCf05KXf3ADNOy5o/bHZ0GWv8pb4hae1GGMrg\n".
+                "ILNkdqlw0m9cuUmpOOuoIvsOnwnS9TC18dHYWHhUk8MTOIhdX49aY04FszdbF99A\n".
+                "k48ooDmyxPEdl3ANIllDzafIJkCbWRO2M1MlllsVAoGARirjlsHqUTbSOr4SWv24\n".
+                "3ZpDCaQgYQxR1DBDpOkpxEjfKVWCgy14S+UWcwrUO86mvhsLnx1BspJtODbUeSJT\n".
+                "CyWARzqVUSh2D99exqfh6599dcfaYzEGBLqYphThWDC5gZC6Hp3r6nSB5aufV+MU\n".
+                "bCFefH7zscNW46N5gRD/O8ECgYEAiJ59J/aT8anQXZwv/JMqudADWTZTvb+z4qMd\n".
+                "FQ4jOAY6/bXhzgK5wCBK+Jw4OiEDbElXAti1wWphBlgFx2LbWUwhi2ycrqHeabxy\n".
+                "eQHQKM8DbaPoXkPhC/oar2zZCRTM2G59EZMCimfy4jWG/FRldyCQm8Y3H6XdvETY\n".
+                "G6wMMpkCgYEAlQflexC/PEJpgDXrz8van3s8uVodygxwyZIa8/6Lpzb+fVzI3Ovz\n".
+                "uBYXxfB1jozVcEovt2/G5flthGpslh3frLZPjT+kA96cT5FKk9QoNsBhFfnyskeY\n".
+                "jY3tCHjL8ZjDSJ7Jvf4Vdjd8MR/bR93qxjN9a9gH6Hdu0taW1G2lCJ0=\n".
+                "-----END RSA PRIVATE KEY-----";
+
+            $publicCertWebPay = "-----BEGIN CERTIFICATE-----\n".
+                "MIIDUTCCAjkCFH6499HVMytWRm08jPaZlJfT8wQ8MA0GCSqGSIb3DQEBCwUAMGUx\n".
+                "CzAJBgNVBAYTAkNMMRMwEQYDVQQIDApTb21lLVN0YXRlMREwDwYDVQQHDAhTQU5U\n".
+                "SUFHTzEXMBUGA1UECgwORURVQ0FDSU9OQ0hJTEUxFTATBgNVBAMMDDU5NzAzNTAy\n".
+                "OTU3NTAeFw0xOTExMTgwMTQyMjNaFw0yMzExMTcwMTQyMjNaMGUxCzAJBgNVBAYT\n".
+                "AkNMMRMwEQYDVQQIDApTb21lLVN0YXRlMREwDwYDVQQHDAhTQU5USUFHTzEXMBUG\n".
+                "A1UECgwORURVQ0FDSU9OQ0hJTEUxFTATBgNVBAMMDDU5NzAzNTAyOTU3NTCCASIw\n".
+                "DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMjghjzArooOZfXBFiStEldl8+E\n".
+                "FwWAIFq/ID711EJ7g9AMU5XTWcuGqTID8ROZ+aG6jbOXNPTksYaTmpw3yuOBEckq\n".
+                "bNh1Q34qzXo1hy+QDzfx10D7IaNXXoTdlb+cvAP0/bcqavn7uQ1taMWNioHMxnZO\n".
+                "jfD+BWaG+oygs1JQkrFzTC/EZvM0GujJtXBq/ZiE7Wz7KVumR4VqCe093uagSmgX\n".
+                "nxiKVeD4mtovub2TCzlhzHamBRKu47Od1OhsegqNLyzBW2NycqBD3WPfFb5El03q\n".
+                "x5IHcBA+22ioVA1xkQiyeKqwIYQS5to0l8JUvIfEQGkvFMBxoGFVaneVPxcCAwEA\n".
+                "ATANBgkqhkiG9w0BAQsFAAOCAQEAYgkXBvh8HiVDPFHRROyd3UClx1aJAmKa3/+P\n".
+                "6Hx1qSvVy0BQB1m4BerFuDlMubGQnlmCpGhyHtXKHHkkGd4MYPaIJV5CF9F1pA57\n".
+                "gQC8uxGh0lvK4sk0pj1cHf4QEkvrLz8oMPPff2c79ZECG7DvrpH+vH0fz3pPtcZ3\n".
+                "xDKhIOtMHfja2/eAVLXU2KVxbmc6uJjvLfuO2syFalubENgx408JXomH6mmMQB0W\n".
+                "9YuqCq14EPcaYN7i/YGz/zeykxgqpJgHuDNGE7DgbdUzrEClCqq3+WDSjT7GLnFf\n".
+                "EEzJAdqtLOWc0gca1sngjYa1fP6cU+0aQ0USQHf1Bv9LCrxbMg==\n".
+                "-----END CERTIFICATE-----";
+
 
             $configuration->setCommerceCode($commerceCode);
             $configuration->setPrivateKey($privateKeyWebPay);
