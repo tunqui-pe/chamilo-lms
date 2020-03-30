916625e562b0fb4923f31cbc4b0532bdc7aa274b
Alex Aragón <aragcar@gmail.com>
2020-03-30 15:47:31 -0500

    add lang spanish2

---
+++ main/lang/spanish2/trad4all.inc.php
@@ -0,0 +1,27 @@
+<?php
+$Phone="Teléfono o Celular";
+$SrcPhone="Teléfono o Celular";
+$ChangePasswordCurrent= "Si deseas cambiar su contraseña, deberás escribir tu contraseña actual, de lo contrario dejar el campo vacío";
+$EnterNewPassword="Ingresar tu nueva contraseña, solo si deseas cambiarla";
+$RepeatYouNewPassword="Repite tu nueva contraseña ingresada anteriormente";
+$helloUser='Hola!';
+$howAreYour='¿cómo estás?';
+$textWelcome='Te queremos dar la más cordial bienvenida a nuestra plataforma online de Educación Chile, esperamos que nuestros cursos y diplomados sean de gran utilidad para tu quehacer laboral y profesional.';
+$accessCredentials='Tus datos de acceso son los siguientes';
+$userNameEmail='Nombre de usuario/email';
+$technicalSupport='Cualquier duda técnica nos puedes escribir al email';
+$regardsEducaChile="Saludos.<br> EQUIPO ONLINE<br>Educación Chile";
+$linkPlataform="Link";
+$powerPlatform="Enviado desde Plataforma aula-virtual.cl";
+$CheckEmailResetPassword= "Ingresa tu nueva contraseña para el acceso a tu plataforma.";
+$GoToCourseSession="Ir a mis cursos";
+$hopingYouAreWell="Te queremos informar que te encuentras inscrito(a) en";
+$TheLinkAccess="El enlace para acceder es";
+$AnyTechnicalQuestions="Cualquier duda técnica nos puedes escribir al email";
+$subjectSession="Bienvenido(a), tu inscripción ha sido completada.";
+$toWelcome="te damos la bienvenida!";
+$CopyScheduledAnnouncements = "Copiar anuncios programados";
+$NoScheduledAnnouncements = "La sesión no tiene anuncios programados";
+$CopyConfirmation = "Los anuncios fueron copiados correctamente en la sessión seleccionada";
+$UserReportInSession = "Informe de usuarios registrados en sesión";
+$diplomaCourses= "Cursos del diplomado";
