f703128601fdc4eed0c67cf975c82dce44df32ca
Alex Aragón <aragcar@gmail.com>
2020-05-18 15:00:56 -0500

    add pugin zoom

---
+++ plugin/zoom/src/zoom_plugin.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin class for the Zoom Conector plugin.
+ *
+ * @package chamilo.plugin.zoom
+ *
+ * @author Alex Aragón Calixto    <alex.aragon@tunqui.pe>
+ */
+class ZoomPlugin extends Plugin
+{
+
+
+}
